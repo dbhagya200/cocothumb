0566d71791abb994fcf31820b9b9f68b430c2e97
dbhagya200 <dilinibhagya53@gmail.com>
2024-05-06 15:41:47 +0530

    update all4

--- src/main/java/lk/ijse/cocothumb/controller/UserFormController.java
+++ src/main/java/lk/ijse/cocothumb/controller/UserFormController.java
@@ -3,27 +3,43 @@ package lk.ijse.cocothumb.controller;
 import com.jfoenix.controls.JFXComboBox;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import lk.ijse.cocothumb.model.Customer;
-import lk.ijse.cocothumb.model.Job;
-import lk.ijse.cocothumb.model.User;
-import lk.ijse.cocothumb.repository.CustomerRepo;
-import lk.ijse.cocothumb.repository.EmployeeRepo;
-import lk.ijse.cocothumb.repository.UserRepo;
-import lk.ijse.cocothumb.repository.JobRepo;
-
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class UserFormController {
 
+    @FXML
+    private JFXComboBox<?> cmbJobrole;
+
+    @FXML
+    private AnchorPane rootNodeUserForm;
+
+    @FXML
+    private TextField txtConfirm;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtEmpId;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUserId;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnConfirm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cmbJobAction(ActionEvent event) {
+
+    }
 
 }
